f025518f8a0fa4fcbc484588ab853a957d5745de
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-08 11:19:00 -0800

    Fixed Routing issue

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function GetGameID({ playerID, setGameID }) {
+	let navigate = useNavigate();
 	useEffect(() => {
 		if (playerID) {
 			const url = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${playerID}/ids?start=0&count=5&api_key=${process.env.REACT_APP_TEAM_KEY}`;
@@ -8,6 +10,7 @@ export default function GetGameID({ playerID, setGameID }) {
 				.then((response) => response.json())
 				.then((data) => {
 					setGameID(data);
+					navigate(`/game/${0}`);
 				})
 				.catch(console.error);
 		}
