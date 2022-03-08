659b984089d7be694f5ab9f44c4a1cfc41b1adad
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-08 12:32:44 -0800

    Added error handling

--- src/Components/FindPlayer.js
+++ src/Components/FindPlayer.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 
 export default function FindPlayer({ setPlayerID }) {
 	const [idInput, setIdInput] = useState('');
+	const [errors, setErrors] = useState('');
 	function getPlayerID() {
 		const url = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${idInput}?api_key=${process.env.REACT_APP_TEAM_KEY}`;
 		fetch(url)
 			.then((response) => response.json())
 			.then((data) => {
 				setPlayerID(data.puuid);
+				setErrors('');
 			})
-			.catch(console.error);
+			.catch((error) => {
+				console.error(error);
+				setErrors('Invalid Summoner');
+			});
 	}
 
 	function handleChange(e) {
@@ -33,6 +38,7 @@ export default function FindPlayer({ setPlayerID }) {
 				required
 				value={idInput}
 			/>
+			{errors ? <p>{errors}</p> : null}
 			<button type='submit' id='search-button'>
 				Find Summoner
 			</button>
