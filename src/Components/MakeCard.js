ee87b290b4db3886072c76cbef915b58b9690fe0
brockpaperscissors <michaeljbrock91@gmail.com>
2022-03-08 12:10:56 -0800

    Fixed render issue with specific champions

--- src/Components/MakeCard.js
+++ src/Components/MakeCard.js
@@ -24,8 +24,13 @@ export default function MakeCard({ playerDetails, gameID, setGameDetails }) {
 							<header id='card-header'>
 								<h1 id='sumName'>{playerDetails.summonerName}</h1>
 								<h2 id='team'>Team: {playerDetails.teamId / 100}</h2>
+
 								<img
-									src={`http://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${playerDetails.championName}.png`}
+									src={`http://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${
+										playerDetails.championName === 'FiddleSticks'
+											? 'Fiddlesticks'
+											: playerDetails.championName
+									}.png`}
 									alt={`${playerDetails.championName}`}
 									id='player-champ'
 								/>
